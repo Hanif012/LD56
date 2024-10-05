673335f8afea010636bbc5c3a3da051fbef1da3e
RB2109 <113221386+RB2109@users.noreply.github.com>
2024-10-05 21:04:41 +0800

    Loader file

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManager;
+public static class Loader 
+{
+
+    public enum Scene
+    {
+
+    }
+   public static void Load()
+   {
+    SceneManager.LoadScene("");
+   }
+}
