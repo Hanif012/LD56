2f637ad63db25bfe6664a2d4ccc36e7428f324ef
Hanif <andque12345012@gmail.com>
2024-10-08 00:52:53 +0700

    Rearrange Menu might comback later

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Scene NextScene;
     public void PlayGame()
     {
-        
+        SceneManager.LoadScene(NextScene.buildIndex);
     }
 }
