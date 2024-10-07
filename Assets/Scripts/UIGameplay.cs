8e0b9d9c38e72d4ad6becd98f156c77e612c63af
Hanif <andque12345012@gmail.com>
2024-10-08 00:21:52 +0700

    Add French House and Light

---
+++ Assets/Scripts/UIGameplay.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIGameplayManager : MonoBehaviour
+{
+    [SerializeField] private GameObject uiPause;
+    [SerializeField] private GameObject uiGameOver;
+    [SerializeField] private GameObject uiGameplay;
+}
